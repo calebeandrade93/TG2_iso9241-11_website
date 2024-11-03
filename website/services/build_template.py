566bec5f677c6c874fe7deae9eaa099560e37d40
calebeandrade93 <andrade.calebe@outlook.com>
2024-11-03 17:35:09 -0300

    vsf

--- website/services/build_template.py
+++ website/services/build_template.py
@@ -1,19 +1,27 @@
-
 class BuildTemplate:
 
     def build(questions, answers):
         template = {}
+        answer_dict = {str(answer.get('question')).strip(): answer for answer in answers}
+        
         for question in questions:
             question_id = str(question.get('_id')).strip()
-            for answer in answers:
-                answer_question = str(answer.get('question')).strip()
-                if question_id == answer_question:
-                    module = question.get('module')
-                    if module not in template:
-                        template[module] = []
-                    template[module].append({
-                        question.get('description'): answer.get('answer'),
-                        "notes": answer.get('notes'),
-                        "glossary": question.get('glossary')
-                    })
+            module = question.get('module')
+            if module not in template:
+                template[module] = []
+            
+            answer = answer_dict.get(question_id, None)
+            if answer:
+                template[module].append({
+                    question.get('description'): answer.get('answer'),
+                    "notes": answer.get('notes'),
+                    "glossary": question.get('glossary')
+                })
+            else:
+                template[module].append({
+                    question.get('description'): None,
+                    "notes": "",
+                    "glossary": question.get('glossary')
+                })
+        
         return template
